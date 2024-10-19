dd4c07ab5990eb3f8aef675295c3f49b4b3cb8aa
Javier Gascon <javitocatral@gmail.com>
2024-10-19 20:13:03 +0200

    animacion y huella

--- src/pages/PrivateArea.jsx
+++ src/pages/PrivateArea.jsx
@@ -1,38 +1,37 @@
-import princesaGalactica from "../assets/avatar.assets/princesaGalactica.png"
-import recicledBoy from "../assets/avatar.assets/recicledBoy.png"
-import recicledGollum from "../assets/avatar.assets/recicledGollum.png"
-import SuperNature from "../assets/avatar.assets/SuperNature.png"
-import mrBotellita from "../assets/avatar.assets/mrBotellita.png"
-import niñaRama from "../assets/avatar.assets/niñaRama.png"
-import neoShrek from "../assets/avatar.assets/neoShrek.png"
-import emoNature from "../assets/avatar.assets/emoNature.png"
-import hipstree from "../assets/avatar.assets/hipstree.png"
-import "../css/areaprivada.css"
-import NavBar from "../components/NavBar"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import services from "../services/config"
-
+import princesaGalactica from '../assets/avatar.assets/princesaGalactica.png'
+import recicledBoy from '../assets/avatar.assets/recicledBoy.png'
+import recicledGollum from '../assets/avatar.assets/recicledGollum.png'
+import SuperNature from '../assets/avatar.assets/SuperNature.png'
+import mrBotellita from '../assets/avatar.assets/mrBotellita.png'
+import niñaRama from '../assets/avatar.assets/niñaRama.png'
+import neoShrek from '../assets/avatar.assets/neoShrek.png'
+import emoNature from '../assets/avatar.assets/emoNature.png'
+import hipstree from '../assets/avatar.assets/hipstree.png'
+import '../css/areaprivada.css'
+import NavBar from '../components/NavBar'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import services from '../services/config'
+import CalculoHuella from '../utils/CalculoHuella'
+import AnimacionAvatar from '../components/AnimacionAvatar'
 
 function PrivateArea() {
   const navigate = useNavigate()
-  const [img, setImg] = useState("")
+  const [img, setImg] = useState('')
   const [dataUser, setDataUser] = useState({})
   const [mediaHuella, setMediaHuella] = useState(0)
 
-  useEffect(()=>{
-      handleGetUser();
-
+  useEffect(() => {
+    handleGetUser()
   }, [])
 
-  const handleGetUser = async () =>{
+  const handleGetUser = async () => {
     try {
       const response = await services.get(`/user`)
-      console.log(response.data)
+
       setDataUser(response.data)
-      const numHuella = await handleMediaHuella(response.data.huella); 
-      setMediaHuella(numHuella);
-      console.log(numHuella)
+      const numHuella = await handleMediaHuella(response.data.huella)
+      setMediaHuella(numHuella)
     } catch (error) {
       if (error.response.status === 400) {
         setErrorMesage(error.response.data.message)
@@ -42,77 +41,68 @@ function PrivateArea() {
     }
   }
 
+  const handleMediaHuella = (array) => {
+    if (!array || array.length === 0) return 0
+    const sumar = array.reduce((acu, valor) => acu + valor, 0)
+    return sumar / array.length
+  }
+
+  if (dataUser.huella === undefined) {
+    return <h3>Loading</h3>
+  }
   // const handleChangeImg = async () =>{
   //   try {
-  //     const img = mrBotellita 
+  //     const img = mrBotellita
   //     await services.patch("/user", img)
-      
+
   //   } catch (error) {
-      
+
   //   }
-  
+
   // }
- 
 
-  const handleMediaHuella =  (array) => {
-    if (!array || array.length === 0) return 0; 
-    const sumar = array.reduce((acu, valor) => acu + valor, 0);
-    return sumar / array.length; 
-  };
-     
-
-  if(dataUser.huella === undefined){
-    return (<h3>Loading</h3>)
-  }
-  
-  const imgAvatar = (img) =>{
-
-    if(img === "mrBotellita"){
+  const imgAvatar = (img) => {
+    if (img === 'mrBotellita') {
       return mrBotellita
-    }else if( img === "emoNature"){
+    } else if (img === 'emoNature') {
       return emoNature
-    }else if ( img === "hipstree"){
+    } else if (img === 'hipstree') {
       return hipstree
-    }else if (img === "neoShrek"){
+    } else if (img === 'neoShrek') {
       return neoShrek
-    }else if (img === "niñaRama"){
+    } else if (img === 'niñaRama') {
       return niñaRama
-    }else if (img === "princesaGalactica"){
+    } else if (img === 'princesaGalactica') {
       return princesaGalactica
-    }else if (img === "recicledBoy"){
+    } else if (img === 'recicledBoy') {
       return recicledBoy
-    }else if (img === "recicledGollum"){
+    } else if (img === 'recicledGollum') {
       return recicledGollum
-    }else if (img === "SuperNature"){
+    } else if (img === 'SuperNature') {
       return SuperNature
     }
-    
   }
-  const avatar =imgAvatar(dataUser.img)
+  const avatar = imgAvatar(dataUser.img)
 
   return (
-    <div className='conatainer-areaprivada'>
-      <NavBar/>
+    <div className="conatainer-areaprivada">
+      <NavBar />
+      <AnimacionAvatar />
+      <div className="avatar">
+        <img style={{ width: '250px' }} src={avatar} alt="avatar" />
+      </div>
+      <div className="tu-huella">
+        <h1>Es tu Huella de hoy:{dataUser.huella[0]}</h1>
+        <h3> es tu media {mediaHuella}</h3>
+        <p>sigue mejorando</p>
+      </div>
 
-
-<div className='avatar'>
-  <img style={{width:'250px'}} src={avatar} alt="avatar" />
-</div>
-<div className="tu-huella">
-  <h1>Es tu Huella de hoy:{dataUser.huella[0]}</h1>
-  <h3> es tu media {mediaHuella}</h3>
-  <p>sigue mejorando</p>
-</div>
-
-<div className="info-avatar">
-  
-  <h3>Name:  {dataUser.name}</h3>
-  <h4>Username: {dataUser.username}</h4>  <h4>mail:{dataUser.email} </h4>
-
-</div>
-
-
-    </div> 
+      <div className="info-avatar">
+        <h3>Name: {dataUser.name}</h3>
+        <h4>Username: {dataUser.username}</h4> <h4>mail:{dataUser.email} </h4>
+      </div>
+      <CalculoHuella />
+    </div>
   )
 }
 
--- src/utils/CalculoHuella.jsx
+++ src/utils/CalculoHuella.jsx
@@ -1,52 +1,218 @@
+import services from '../services/config'
+import { useEffect, useState, useContext } from 'react'
 
-// import services from "../services/config"
-// import { useEffect, useState, useContext } from "react"
-// import { AuthContext } from "../context/auth.context"
-// import { Navigate, useNavigate } from "react-router-dom"
+function CalculoHuella() {
+  const [dataHabitos, setDataHabitos] = useState([])
 
+  useEffect(() => {
+    GetDataHabitos()
+  }, [])
+  const factoresEmision = {
+    pollo: 6.9,
+    ternera: 27,
+    vegetales: 2,
+    cerdo: 12.1,
+    pescado: 6,
+  }
+  const factoresEmisionTransporte = {
+    coche: {
+      gasolina: 192,
+      diesel: 171,
+      electrico: 0,
+      hibrido: 122,
+    },
+    autobus: 104,
+    tren: 41,
+    metro: 29,
+    bicicleta: 0,
+    caminar: 0,
+    avion: 285,
+  }
+  const velocidadesPromedio = {
+    coche: 60,
+    autobus: 40,
+    tren: 80,
+    metro: 30,
+    bicicleta: 15,
+    caminar: 5,
+    avion: 800,
+  }
+  // esto es kilometro hora
 
-// function CalculoHuella() {
+  const factoresEmisionEnergia = {
+    electricidad: 0.233,
+    gasNatural: 0.185,
+    butano: 0.25,
+  }
+  // kg CO2 por kWh
 
+  // Conversión de butano a kWh
+  const conversionButanoKwh = 13.75 // kWh por kg de butano
 
-//   const [dataHabitos, setDataHabitos]=useState([])
-  
+  // Consumos promedio anuales para un hogar en España (en kWh)
+  const consumoPromedioDiario = {
+    electricidad: 3400 / 365, // kWh por día
+    gasNatural: 10000 / 365, // kWh por día
+    butano: (100 * conversionButanoKwh) / 365, // Convertir kg a kWh y luego a diario
+  }
+  const descuentoRenovable = 0.5
+  const huellaResiduosPorPersona = 1
+  const GetDataHabitos = async () => {
+    try {
+      const response = await services.get(`huella/user`)
+      console.log(response.data)
+      setDataHabitos(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const hoy = new Date()
+  const fechaHoy = hoy.toISOString().split('T')[0]
+  const transportes = dataHabitos.filter((cadaHabito) => {
+    return cadaHabito.transporte && cadaHabito.createdAt.startsWith(fechaHoy)
+  })
+  const alimentacion = dataHabitos.filter((cadaHabito) => {
+    return cadaHabito.alimentacion && cadaHabito.createdAt.startsWith(fechaHoy)
+  })
+  const otros = dataHabitos.filter((cadaHabito) => {
+    return cadaHabito.otros && cadaHabito.createdAt.startsWith(fechaHoy)
+  })
+  console.log(otros)
+  function calcularHuellaAlimento(alimento, cantidad, esDeProximidad) {
+    let cantidadKg = cantidad / 1000
+    let factor = factoresEmision[alimento]
 
-  
-//   useEffect(()=>{
-//     GetDataHabitos()
-//     //handleMediaHuella()
-//   }, [])
+    let huellaCarbono = cantidadKg * factor
 
-//   const GetDataHabitos = async () =>{
-//     try {
-//       const response =await services.get(`huella/user`)
-//       console.log(response.data)
-//       setDataHabitos(response.data)
-      
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
+    if (esDeProximidad) {
+      huellaCarbono *= 0.9
+    }
+    return huellaCarbono
+  }
 
-//  //   const patchMediHuella = async () =>{
+  let huellaTotalAlimento = 0
+  alimentacion.forEach((cadaHabito) => {
+    const { alimento, cantidad, esDeProximidad } = cadaHabito.alimentacion
+    huellaTotalAlimento += calcularHuellaAlimento(
+      alimento,
+      cantidad,
+      esDeProximidad
+    )
+  })
 
-//     //   try {
-        
-//     //   } catch (error) {
-        
-//     //   }
+  function calcularHuellaTransporte(vehiculo, tiempo, motor = null) {
+    const velocidad = velocidadesPromedio[vehiculo]
+    if (!velocidad) {
+      console.error(`Tipo de vehículo no válido: ${vehiculo}`)
+      return 0
+    }
 
-//     // }
-// //  const handleMediaHuella = (array)=>{
+    const distanciaKm = (tiempo / 60) * velocidad
 
-// //     const sumar =array.reduce((acu, valor)=> acu + valor, 0)
-// //     return sumar / array.length
-  
-// //   }
+    let huellaCarbono
 
-//   return (
-//     <div>CalculoHuella</div>
-//   )
-// }
+    if (vehiculo === 'coche') {
+      if (!motor) {
+        console.error('Se requiere especificar el tipo de motor para el coche.')
+        return 0
+      }
 
-// export default CalculoHuella
+      const factor = factoresEmisionTransporte.coche[motor]
+      if (!factor) {
+        console.error(`Tipo de motor no válido: ${motor}`)
+        return 0
+      }
+
+      huellaCarbono = distanciaKm * factor
+    } else {
+      const factor = factoresEmisionTransporte[vehiculo]
+      if (!factor) {
+        console.error(`Tipo de vehículo no válido: ${vehiculo}`)
+        return 0
+      }
+
+      huellaCarbono = distanciaKm * factor
+    }
+
+    return huellaCarbono / 1000
+  }
+
+  function calcularHuellaGeneral(transportes) {
+    let huellaTotal = 0
+
+    transportes.forEach((cadaHabito) => {
+      console.log(cadaHabito)
+      const vehiculo = cadaHabito.transporte.vehiculo
+      const tiempo = cadaHabito.transporte.tiempo
+      const motor = cadaHabito.transporte.motor
+
+      huellaTotal += calcularHuellaTransporte(
+        vehiculo,
+        tiempo,
+        vehiculo === 'coche' ? motor : null
+      )
+    })
+    return huellaTotal
+  }
+  const huellaGeneraltransportes = calcularHuellaGeneral(transportes)
+
+  function calcularHuellaEnergiaDiaria(consumo) {
+    let huellaTotal = 0
+
+    consumo.forEach((fuente) => {
+      const { consumoEnergetico, esRenovable, recicla } = fuente.otros
+      const factor = factoresEmisionEnergia[consumoEnergetico]
+      const cantidadDiaria = consumoPromedioDiario[consumoEnergetico]
+
+      if (!factor) {
+        console.error(`Tipo de energía no válido: ${consumoEnergetico}`)
+        return
+      }
+
+      if (!cantidadDiaria) {
+        console.error(
+          `No hay datos de consumo promedio para: ${consumoEnergetico}`
+        )
+        return
+      }
+
+      // Calcular huella diaria
+      let huella = cantidadDiaria * factor // Huella diaria
+
+      // Descuento si es renovable
+      if (esRenovable) {
+        huella *= 0.9 // Descuento del 10% para energías renovables
+      }
+
+      // Sumar a la huella total
+      huellaTotal += huella
+
+      // Si no recicla, añadir huella de residuos
+      if (!recicla) {
+        huellaTotal += huellaResiduosPorPersona // Asegúrate de definir esto en otro lugar
+      }
+    })
+
+    return huellaTotal
+  }
+
+  const huellaDiariaenergia = calcularHuellaEnergiaDiaria(otros)
+
+  const huellaTotal =
+    huellaDiariaenergia + huellaGeneraltransportes + huellaTotalAlimento
+  console.log(
+    `Huella de carbono total diaria: ${huellaTotal.toFixed(2)} kg CO₂`
+  )
+  /* const patchMediHuella = async () => {
+    try {
+    } catch (error) {}
+  }
+  const handleMediaHuella = (array) => {
+    const sumar = array.reduce((acu, valor) => acu + valor, 0)
+    return sumar / array.length
+  }*/
+
+  return <div>CalculoHuella</div>
+}
+
+export default CalculoHuella
